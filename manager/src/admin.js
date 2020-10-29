06d9472706fc2d69c40a05ffb6e06b8ac6d4de3c
李标泉 <1083756620@qq.com>
2020-10-29 17:54:25 +0800

    搭建环境配置

---
+++ manager/src/admin.js
@@ -0,0 +1,26 @@
+import { Col, Row } from 'antd';
+import React, {Component} from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import NavLeft from './components/NavLeft'
+import './style/common.less'
+
+export default class Admin extends Component{
+    render(){
+        return (
+            <Row className="container">
+                <Col span="3" className="nav-left">
+                <NavLeft/>
+                </Col>
+                <Col span="21" className="main">
+                    <Header/>
+                    <Row className="content">
+                        {/* {this.props.children} */}
+                         content
+                         </Row>
+                    <Footer/>
+                </Col>
+            </Row>
+        )
+    }
+}
---
+++ manager/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+export default class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                this is Header
+            </div>
+        )
+    }
+}
